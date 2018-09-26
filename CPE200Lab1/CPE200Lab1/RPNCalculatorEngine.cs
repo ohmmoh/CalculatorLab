f260b193562323c4e4ef5257a741988bedfafcbe
isne505 <jetdilok_ng@cmu.ac.th>
2018-09-26 17:39:34 +0700

    All test pass in clude invalidform

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -17,14 +17,17 @@ namespace CPE200Lab1
             List<string> parts = str.Split(' ').ToList<string>();
             string result;
             string firstOperand, secondOperand;
-            if(parts.Count == 1&&parts.Count == 0)
+            if(parts.Count == 1&& parts[0]!="0")
             {
                 return "E";
             }
            
             foreach (string token in parts)
             {
-                
+                if (parts.Count == 2)
+                {
+                    return "E";
+                }
                 if (isNumber(token))
                 {
                     rpnStack.Push(token);
@@ -32,21 +35,43 @@ namespace CPE200Lab1
                 else if (isOperator(token))
                 {
                     //FIXME, what if there is only one left in stack?
-                    
+                   try
+                    { 
                     secondOperand = rpnStack.Pop();
                     firstOperand = rpnStack.Pop();
-                    result = calculate(token, firstOperand, secondOperand, 6);
+                    result = calculate(token, firstOperand, secondOperand, 6);}
+                    catch (Exception)
+                    {
+                        result = "E";
+                    }
                     if (result is "E")
                     {
                         return result;
                     }
                     rpnStack.Push(result);
+                }else if(token=="")
+                {
+
+                }
+                else  
+                {
+                    return "E";
                 }
                
             }
             //FIXME, what if there is more than one, or zero, items in the stack?
-            result = rpnStack.Pop();
-            return result;
+            
+            if(rpnStack.Count==1)
+            {
+            
+                result = rpnStack.Pop();
+                return result;
+            }
+            else
+            {
+                return "E";
+            }
+          
 
         }
     }
